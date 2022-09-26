30233ae4cad6296f6ef23ef1c511a9c659bd547a
용환 <lyhlyhlyhlyhlyh@daum.net>
2022-09-27 00:14:40 +0900

    C

--- DealWithWEb/src/main/java/servlet/sec01/ex03/HeaderInfoServlet.java
+++ DealWithWEb/src/main/java/servlet/sec01/ex03/HeaderInfoServlet.java
@@ -2,7 +2,6 @@ package servlet.sec01.ex03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/headerinfo")
-public class URLInfoServlet extends HttpServlet {
+@WebServlet("/urlinfo")
+public class HeaderInfoServlet extends HttpServlet {
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -24,12 +23,12 @@ public class URLInfoServlet extends HttpServlet {
 		out.print("<title>Request 정보 출력 Servlet</title>");
 		out.print("</head>");
 		out.print("<body>");
-		out.print("<h3>요청 헤더 정보</h3>");
-		Enumeration<String> em = req.getHeaderNames(); //모든 헤더 이름 가져오는 메서드
-		while(em.hasMoreElements()) {
-			String s = em.nextElement();
-			out.print(s+" : "+req.getHeader(s)+"<br/>");
-		}
+		out.print("<h3>요청 방식과 프로토콜 정보</h3>");
+		out.print("Request URI : "+req.getRequestURI()+"<br/>"); // 포트번호 뒤에부터 경로
+		out.print("Request URL : "+req.getRequestURL()+"<br/>"); //전체 url
+		out.print("Context Path : "+req.getContextPath()+"<br/>"); //웹 애플리케이션 경로 정보
+		out.print("Request Protocol : "+req.getProtocol()+"<br/>"); // 프로토콜 방식
+		out.print("Servlet Path : "+req.getServletPath()); //서블릿 경로
 		out.print("</body>");
 		out.print("</html>");
 		
